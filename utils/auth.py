c1c4f97af8ddbe29b23ec0292013add65c239c26
Tayyab <tayyabgan57@gmail.com>
2025-01-14 18:08:02 +0500

    update_users

--- utils/auth.py
+++ utils/auth.py
@@ -1,16 +1,26 @@
 users = [
     {
-        "email": "senteze@yopmail.com",
+        "email": "senteze1@yopmail.com",
         "password": "Pass1234@",
         "role": "employee"
     },
     {
-        "email": "fascicoli@yopmail.com",
+        "email": "senteze2@yopmail.com",
+        "password": "Pass1234@",
+        "role": "employee"
+    },
+    {
+        "email": "fascicoli1@yopmail.com",
         "password": "Pass1234@",
         "role": "manager"
     },
     {
-        "email": "affairs@astutesoftwares.com",
+        "email": "fascicoli2@yopmail.com",
+        "password": "Pass1234@",
+        "role": "manager"
+    },
+    {
+        "email": "superadmin@yopmail.com",
         "password": "Pass1234@",
         "role": "admin"
     },
@@ -22,7 +32,3 @@ def authenticate(email: str, password: str):
         return list(filter(lambda x: x["email"] == email and x["password"] == password, users))[0]
     except:
         return None
-
-
-
-
--- utils/embedder.py
+++ utils/embedder.py
@@ -38,7 +38,7 @@ def create_embeddings(file_path: str, role):
 
     vector_store = PGVector(
         embeddings=embeddings,
-        collection_name="law-index-v3",
+        collection_name=os.getenv("collection_name"),
         connection=os.getenv("connection"),
         use_jsonb=True,
     )
